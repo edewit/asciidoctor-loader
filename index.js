fbe40655e2e793a464ac8f69056865ded451dc76
Erik Jan de Wit <erikjan.dewit@gmail.com>
2017-04-18 10:55:28 +0200

    use index file to load external content

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var loaderUtils = require("loader-utils");
 var assign = require("object-assign");
 var asciidoctor = require('asciidoctor.js')();
+var requestSync = require("sync-request");
 
 // default option
 var defaultOptions = {
@@ -11,11 +12,28 @@ var defaultOptions = {
 };
 
 module.exports = function (content) {
+
   // merge params and default config
   var query = loaderUtils.parseQuery(this.query);
   var options = assign({}, defaultOptions, query, this.options["asciidoctorLoader"]);
+  
+  try {
+    content = JSON.parse(content);
+  } catch (e) {
+    throw new Error('The .index file is not a valid json. ' + e.toString());
+  }
+
+  for (var k in content) {
+    if (content.hasOwnProperty(k)) {
+      let url = content[k];
+      var res = requestSync('GET', url);
+      var rawContent = res.getBody('utf8');
+
+      var doc = asciidoctor.convert(rawContent, options);
+      content[k] = doc;
+    }
+  }
 
   this.cacheable();
-
-  return asciidoctor.convert(content, options);
+  return `module.exports = ${JSON.stringify(content)};`;
 };
